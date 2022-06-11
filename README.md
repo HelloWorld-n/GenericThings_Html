087648d07d1be0f7667e29ae00314d52f83ecb6d
HelloWorld-n <gajica.b@gmail.com>
2022-06-11 11:09:22 +0200

    HI!

---
+++ README.md
@@ -0,0 +1,1 @@
+# GenericThings_Html
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/main.css">
+	<title>Document</title>
+</head>
+<style>
+	html\:h1 {
+		font-size: 60px;
+		text-decoration-skip-ink: none;
+		text-decoration: underline dotted #0D0D14 4px;
+	}
+</style>
+<html:body>
+	Hello, world!
+	<h1>
+		Hello, world! {namespace: None}
+	</h1>
+	<html:h1>
+		Hello, world! {namespace: "html"}
+	</html:h1>
+	<script src="js/main.js"></script>
+</html:body>
+
+</html>
